4084bf8d99ec38dff17bfe5e47daf3f2645e7ab2
BuildTools <unconfigured@null.spigotmc.org>
2020-06-05 14:50:53 -0700

    Added .docx and .pdf support.

--- download.py
+++ download.py
@@ -1,6 +1,7 @@
 import requests as rq
 import bbcode
 from docx import Document
+from pdfminer.high_level import extract_text
 import json, os, configparser
 
 # Create BBCode parser
@@ -28,16 +29,35 @@ except:
     pass
 
 # Initialize downloads.txt.
-with open("downloads.txt", "a", encoding="latin1") as file:
+with open("downloads.txt", "a", encoding="utf8") as file:
     # Loop through posts in gallery.
     for post in gallery:
         # Find the download link.
         request = get(f"http://{link}/submission/{post}.json")
         title = request["title"]
         print("Downloading " + title)
+
         # Check if file is valid text and strip bbcode and save to download.txt, skip it not.
         if request["download"].endswith('.txt'):
             download = rq.get(request["download"])
-            file.write(parser.strip(download.text))
+            text = bytes(parser.strip(download.text), 'latin-1').decode('utf-8')
+            file.write(text)
+        elif request["download"].endswith('.doc') or request["download"].endswith('.docx'):
+            with open('tempdownload.docx', 'wb') as tempfile:
+                download = rq.get(request["download"])
+                tempfile.write(download.content)
+                document = Document('tempdownload.docx')
+                docText = '\n\n'.join(
+                    paragraph.text for paragraph in document.paragraphs
+                )
+            file.write(docText)
+            os.remove("tempdownload.docx")
+        elif request["download"].endswith('.pdf'):
+            with open('tempdownload.pdf', 'wb') as tempfile:
+                download = rq.get(request["download"])
+                tempfile.write(download.content)
+            text = extract_text('tempdownload.pdf')
+            file.write(text)
+            os.remove('tempdownload.pdf')
         else:
             print(f"Submission {title} is not a valid .txt file. It will be skipped.")
