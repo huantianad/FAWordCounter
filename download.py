b937a535798733015ab1aa7c9ffe3faf22737b70
BuildTools <unconfigured@null.spigotmc.org>
2020-06-05 20:35:38 -0700

    Improved comments and readability

--- download.py
+++ download.py
@@ -19,13 +19,13 @@ with open('config.ini') as configfile:
     link = config['Main']['scraper']
 
 
-# Define a function to access the api at url and return parsed json.
+# Define a function to access the api at url and return parsed json
 def get(url):
     object = rq.get(url)
     return json.loads(object.text)
 
 
-# Get submissions of user and store it in gallery.
+# Get submissions of user and store it in gallery
 gallery = get(f"http://{link}/user/{username}/gallery.json")
 print(str(len(gallery)) + " submissions found.")
 
@@ -33,36 +33,46 @@ print(str(len(gallery)) + " submissions found.")
 if os.path.exists("downloads.txt"):
     os.remove("downloads.txt")
 
-# Initialize downloads.txt.
+# Open downloads.txt.
 with open("downloads.txt", "a", encoding="utf8") as file:
-    # Loop through posts in gallery.
+    # Loop through posts in gallery
     for post in gallery:
-        # Find the download link.
+        # Find the download link of post
         request = get(f"http://{link}/submission/{post}.json")
         title = request["title"]
         print("Downloading " + title)
 
-        # Check if file is valid text and strip bbcode and save to download.txt, skip it not.
+        # Check file type and process it accordingly
         if request["download"].endswith('.txt'):
+            # Remove BBCode from .txt file and add it to downloads.txt
             download = rq.get(request["download"])
             text = bytes(parser.strip(download.text), 'latin-1').decode('utf-8')
             file.write(text)
+
         elif request["download"].endswith('.doc') or request["download"].endswith('.docx'):
             with open('tempdownload.docx', 'wb') as tempfile:
+                # Download and write file to a temporary file
                 download = rq.get(request["download"])
                 tempfile.write(download.content)
+                # Load temporary file and convert to raw text
                 document = Document('tempdownload.docx')
                 docText = '\n\n'.join(
                     paragraph.text for paragraph in document.paragraphs
                 )
+            # Write text to downloads.txt and remove temporary file
             file.write(docText)
             os.remove("tempdownload.docx")
+
         elif request["download"].endswith('.pdf'):
             with open('tempdownload.pdf', 'wb') as tempfile:
+                # Download and write file to a temporary file
                 download = rq.get(request["download"])
                 tempfile.write(download.content)
+            # Convert pdf to raw text
             text = extract_text('tempdownload.pdf')
+            # Write text to downloads.txt and remove temporary file
             file.write(text)
             os.remove('tempdownload.pdf')
+
         else:
             print(f"Submission {title} is not a valid file. It will be skipped.")
